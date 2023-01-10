3e3d96a10305a3de27a1c9f04b9a8c89afc04ad0
Jonathan Blandon <jonathanblandonm6gmail.com>
2023-01-10 15:38:29 -0600

    Implementing Mongoose to create sccharacters schema and model, to learn about its funtionalities

--- src/database/main.js
+++ src/database/main.js
@@ -1,9 +1,9 @@
-const mongoose = require("mongoose");
-const debug = require('debug')("appModule-MongooseConnection");
+const { Schema, default: mongoose } = require("mongoose");
+const debug = require("debug")("appModule-MongooseConnection");
 const { Config } = require("../config");
 const dataBase = Config.mongoUri;
 var connection = null;
-const connectMongoose = async(COLLECTION) =>
+const connectMongoose = async (COLLECTION) =>
   new Promise(async (resolve, reject) => {
     try {
       if (!connection) {
@@ -16,12 +16,11 @@ const connectMongoose = async(COLLECTION) =>
         // get the connection object to operate the results
         connection = await mongoose.connection;
         connection.on("error", console.error.bind(console, "Error connecting"));
-        connection.on("open", () => {
+        connection.on("open", async () => {
           debug("DB connected Successfully");
         });
       }
       debug("Connection Already exist");
-
       resolve(await connection.collection(COLLECTION));
     } catch (error) {
       reject(error);

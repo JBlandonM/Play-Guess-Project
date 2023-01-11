aa225a68b4013dab28a074f0d9158dda7597215b
Jonathan Blandon <jonathanblandonm6gmail.com>
2023-01-10 22:48:26 -0600

    folder renamed

--- src/Characters/services.js
+++ src/Characters/services.js
@@ -1,35 +1,35 @@
-const { Schema, default: mongoose } = require("mongoose");
+const { Schema, default: mongoose, mongo } = require("mongoose");
 const { Database } = require("../database/main");
 // const COLLECTION = "pixars";
-// const COLLECTION = "";
-const COLLECTION = "cartoon networks";
-const getAll = async () => { // get match (collection==match)
+const COLLECTION = "Disney";
+// const COLLECTION = "cartoon networks";
+const getAll = async () => {
+  // get match (collection==match)
   const result = await Database.connectMongoose(COLLECTION);
   return await result.find({}).toArray();
 };
 // schema for characters
 const charSchema = new Schema({
   name: String,
-  imageUrl:String,
+  imageUrl: String,
   MOO: String,
 });
-const Chars= mongoose.model(COLLECTION, charSchema); 
+const Chars = mongoose.model(COLLECTION, charSchema, COLLECTION);
 // to add new data on given collection with above schema
-const addChar = async(newData)=>{
+const addChar = async (newData) => {
   const charData = await Database.connectMongoose(COLLECTION);
   const newOne = new Chars({
-    name:newData.name,
-    imageUrl:newData.imageUrl,
-    MOO:newData.MOO,
-  })
+    name: newData.name,
+    imageUrl: newData.imageUrl,
+    MOO: newData.MOO,
+  });
   const result = await newOne.save();
   return result;
 };
-const getCollection =async(COLLECTION)=>{
-  let charCollection = await Database.connectMongoose(COLLECTION)
+const getCollection = async (COLLECTION) => {
+  let charCollection = await Database.connectMongoose(COLLECTION);
   return await charCollection.find({}).toArray();
-
-}
+};
 
 module.exports.CharServices = {
   getAll,

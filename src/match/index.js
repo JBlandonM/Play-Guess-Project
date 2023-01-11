e69cbbbf101885f26587f0317350cc519a1fde4a
Jonathan Blandon <jonathanblandonm6gmail.com>
2023-01-11 16:40:50 -0600

    added the handler

--- src/match/index.js
+++ src/match/index.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router= express.Router();
-const {matchController}=require('./controller');
+const express = require("express");
+const router = express.Router();
+const { matchController } = require("./controller");
 
-module.exports.MatchAPI = (app)=>{
-  router.post("/send", matchController.checkSelected)
-  .get("/point/:pts", matchController.finalView)
-  .get("/startView", matchController.startView)
+module.exports.MatchAPI = (app) => {
+  router
+    .post("/send", matchController.checkSelected)
+    .get("/point/:pts", matchController.finalView)
+    .get("/startView", matchController.startView);
   app.use("/match", router);
 };
-

cfaff33023e74007833897fcee3c3ee37cab6955
Jonathan Blandon <jonathanblandonm6gmail.com>
2023-01-17 22:28:20 -0600

    Added general comments about functionality

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -5,6 +5,7 @@ let imgHistory = [];
 let optionHistory = [];
 let points = 0;
 
+// to get characters data from mongo db, specified by category
 const fetchData = async (category) =>
   new Promise(async (resolve, reject) => {
     try {
@@ -15,6 +16,8 @@ const fetchData = async (category) =>
     }
   });
 
+// this checks the following: the 4 index characters positions that will be inside buttons, might be different
+// it stores 4 different characters names within an array
 const optionToShow = (characters) => {
   while (characters && currentOptions.length < 4) {
     let randomNum = Math.floor(Math.random() * characters.length);
@@ -23,9 +26,9 @@ const optionToShow = (characters) => {
       optionHistory.push(characters[randomNum]);
     }
   }
-  // console.log(`current options: ${currentOptions}`);
 };
 
+// imgToShow uses the array with 4 different characters and returns one, making sure that this one is not already shown before
 const imgToShow = (currentOptions, characters) => {
   try {
     let randomNum = Math.floor(Math.random() * currentOptions.length);
@@ -40,6 +43,7 @@ const imgToShow = (currentOptions, characters) => {
   }
 };
 
+// this unit checks after each dom update, if the user have played 15 "cards": if true, fetch an "endView", and then prints it in browser.
 const endMatch = async (imgHistory) => {
   if (imgHistory.length >= 15) {
     console.log("Match should end");
@@ -59,7 +63,10 @@ const endMatch = async (imgHistory) => {
     console.log("continue");
   }
 };
+
+// this is the main unit of the game, this uses each above unit to handle the match flow
 const update = async () => {
+  // selects from dom the img element to render a character, also the buttons, and a "score"
   var imgElement = document.querySelector(".char");
   let buttonsElements = document.querySelectorAll(".option");
   let pointsCounter = document.querySelector("#score");
@@ -72,6 +79,7 @@ const update = async () => {
   let charToShow = characters[position];
   // console.log(charToShow);
 
+  // inserts the current character image, and further inserts the one different character options in each button element
   imgElement.setAttribute("src", charToShow.imageUrl);
   imgElement.setAttribute("alt", charToShow.name);
   pointsCounter.textContent = points;
@@ -79,14 +87,16 @@ const update = async () => {
   buttonsElements[1].textContent = characters[currentOptions[1]].name;
   buttonsElements[2].textContent = characters[currentOptions[2]].name;
   buttonsElements[3].textContent = characters[currentOptions[3]].name;
+  // from match characters array, "removes" the already displayed char
   characters.splice(position, 1);
-  // console.log("left:", characters);
 
-  currentOptions = [];
+  // restart this state variables before all dom update
+  currentOptions = []; // stores 4 different characters positions to show inside buttons
   optionHistory = [];
   endMatch(imgHistory);
 };
 
+// this checks if the button selected contains the correct answer, if true displays some styles, else displays others
 const checkAnswers = (buttonClicked) => {
   const imageShown = document.querySelector(".char").getAttribute("alt");
   let card = document.querySelector("#card");
@@ -103,13 +113,18 @@ const checkAnswers = (buttonClicked) => {
   }, 700);
 };
 
+// "loading" unit activates and disables a page loader element
 const loading = (display, display0) => {
   document.querySelector("#card").style.visibility = display0;
   document.querySelector("#loader").style.display = display;
 };
 
+// from main index view selects the category buttons and a container element
 const categoryButtons = document.querySelectorAll(".charCatgry");
 const subContainer = document.querySelector("#card");
+
+// creates an onclick event listener for each category, when one is clicked fetch specified data from a category
+// also fetch a matchStart view, this works to display the data while match is played
 var characters = 0;
 categoryButtons.forEach((button) => {
   button.addEventListener("click", async () => {
@@ -126,7 +141,7 @@ categoryButtons.forEach((button) => {
     const buttonOptions = document.querySelectorAll(".option");
 
     update();
-
+    // for each characters displayed and option selected, calls the update function, disables buttons by a second, and calls checkAnswers function
     buttonOptions.forEach((button) => {
       button.addEventListener("click", () => {
         button.disabled = true;

d73ff1ddb0dd698ca32d5a4cae70ac529655fb34
Jonathan Blandon <jonathanblandonm6gmail.com>
2023-01-25 21:21:11 -0600

    added a basis dark mode funtions handler and its styles

--- public/javascripts/test-module.js
+++ public/javascripts/test-module.js
@@ -3,9 +3,26 @@ export const loading = (elemX, stX, elemY, stY) => {
   document.querySelector(elemX).style.display = stX;
   document.querySelector(elemY).style.display = stY;
 };
-export const setFadeOut =() => {
+export const setFadeOut = () => {
   document.querySelector("#card").classList.add("new-card");
   setTimeout(() => {
     document.querySelector("#card").classList.remove("new-card");
   }, 900);
 };
+
+//dark scheme mode handler
+const DarkMode = () => {
+  let condition =
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (condition) {
+    // main color
+    let body = document.querySelector(".main-container");
+    body.style.backgroundColor = "initial";
+    body.style.color = "whiteSmoke";
+    // main card container opacity
+    let card = document.querySelector("#card");
+    card.style.opacity = "0.5";
+  }
+};
+DarkMode();
